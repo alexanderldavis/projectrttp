957dbcd6c59da716dcfeca31b259b70310c600a4
Alexander Davis <alexander.davis54@gmail.com>
2017-05-30 15:01:01 -0500

    change name of users to students

--- init.py
+++ init.py
@@ -10,12 +10,12 @@ url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
 conn = psycopg2.connect(database=url.path[1:],user=url.username,password=url.password,host=url.hostname,port=url.port)
 cur = conn.cursor()
 
-cur.execute("""DROP TABLE IF EXISTS user CASCADE;
+cur.execute("""DROP TABLE IF EXISTS students CASCADE;
                DROP TABLE IF EXISTS professor CASCADE;
                DROP TABLE IF EXISTS game CASCADE;
                DROP TABLE IF EXISTS character CASCADE;""")
 print("TABLES DELETED")
-cur.execute("""CREATE TABLE user (uid serial unique, email varchar(200), name varchar(200), hashpswd varchar(200), gid varchar(100));""")
+cur.execute("""CREATE TABLE students (uid serial unique, email varchar(200), name varchar(200), hashpswd varchar(200), gid varchar(100));""")
 cur.execute("""CREATE TABLE professor (pid serial unique, email varchar(200), hashpswd varchar(200);""")
 cur.execute("""CREATE TABLE game (gid serial unique, title varchar(200), name varchar(200), hashpswd varchar(200), gid varchar(100));""")
 cur.execute("""CREATE TABLE character (cid serial unique, name varchar(200), imageurl varchar(200), description text, objectives text, strategy text, topsecret text);""")
