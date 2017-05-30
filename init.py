c39b2b18227793d57c93a48662c634d44f996597
Alexander Davis <alexander.davis54@gmail.com>
2017-05-30 15:03:05 -0500

    add r paren

--- init.py
+++ init.py
@@ -16,7 +16,7 @@ cur.execute("""DROP TABLE IF EXISTS students CASCADE;
                DROP TABLE IF EXISTS character CASCADE;""")
 print("TABLES DELETED")
 cur.execute("""CREATE TABLE students (uid serial unique, email varchar(200), name varchar(200), hashpswd varchar(200), gid varchar(100));""")
-cur.execute("""CREATE TABLE professor (pid serial unique, email varchar(200), hashpswd varchar(200);""")
+cur.execute("""CREATE TABLE professor (pid serial unique, email varchar(200), hashpswd varchar(200));""")
 cur.execute("""CREATE TABLE game (gid serial unique, title varchar(200), name varchar(200), hashpswd varchar(200), gid varchar(100));""")
 cur.execute("""CREATE TABLE character (cid serial unique, name varchar(200), imageurl varchar(200), description text, objectives text, strategy text, topsecret text);""")
 conn.commit()
