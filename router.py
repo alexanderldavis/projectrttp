61d4c349dd58fcf2d187cb12c86a59e3c6cdb6a9
Alexander Davis <alexander.davis54@gmail.com>
2017-05-31 15:59:16 -0500

    change encrypt

--- router.py
+++ router.py
@@ -48,6 +48,7 @@ def get_hashed_password(plain_text_password):
     return bcrypt.hashpw(plain_text_password, bcrypt.gensalt())
 
 def check_password(plain_text_password, hashed_password):
+    plain_text_password = plain_text_password.encode('utf-8')
     # Check hased password. Using bcrypt, the salt is saved into the hash itself
     return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password)
 
