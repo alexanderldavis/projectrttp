2791fe1f421bc514c2d0564f84c563c2a233c980
Alexander Davis <alexander.davis54@gmail.com>
2017-05-31 15:41:26 -0500

    add encoding for password hash

--- router.py
+++ router.py
@@ -49,7 +49,7 @@ def get_hashed_password(plain_text_password):
 
 def check_password(plain_text_password, hashed_password):
     # Check hased password. Using bcrypt, the salt is saved into the hash itself
-    return bcrypt.checkpw(plain_text_password, hashed_password)
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password)
 
 @app.route("/plogin")
 def loginProfessor():
