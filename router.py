a6cb584dadaf55cc9debaee492d5148e9c4b8c48
Alexander Davis <alexander.davis54@gmail.com>
2017-06-01 14:21:00 -0500

    change professor login last time (separate from game creation)

--- router.py
+++ router.py
@@ -6,7 +6,6 @@ import json
 import requests as req
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
 urllib.parse.uses_netloc.append("postgres")
 url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
 conn = psycopg2.connect(database=url.path[1:],user=url.username,password=url.password,host=url.hostname,port=url.port)
@@ -25,7 +24,6 @@ def createUser():
     name = request.args['name']
     gid = charid.split("-")[0]
     characterid = charid.split("-")[1]
-
     cur.execute("""SELECT * from students where email = %s;""",(email,))
     lst = cur.fetchall()
     if len(lst) == 0:
@@ -43,8 +41,20 @@ def createUser():
     return render_template('main.html', uid = lst)
 
 def hashed_password(password):
-        # return bcrypt.generate_password_hash(password)
         return generate_password_hash(password)
+
+@app.route("/pcreate/<email>/<password>")
+def newProfessor(email, password):
+    cur.execute("""SELECT * from professor where email = %s;""", (email,))
+    lst = cur.fetchall()
+    if len(lst) != 0:
+        return "Professor Account already exists! Please login to your account."
+    hashpassword = hashed_password(password)
+    print("CREATED PASSWORD HASH")
+    cur.execute("""INSERT INTO professor (email, hashpswd) VALUES (%s, %s);""",(email,hashpassword))
+    print("PROFESSOR ACCOUNT CREATED")
+    conn.commit()
+    return "Professor account created!"
 
 @app.route("/plogin/<email>/<password>")
 def loginProfessor(email, password):
@@ -58,27 +68,27 @@ def loginProfessor(email, password):
     return "Professor account not created. Please create an account first."
 
 
-@app.route("/pcreate/<email>/<password>/<gameName>")
-def createProfessor(email, password, gameName):
-    hashpassword = hashed_password(password)
-    print("CREATED PASSWORD HASH")
-
-    # ALREADY EXISTS CHECK
-    cur.execute("""SELECT * from game where title = %s;""", (gameName,))
-    lst = cur.fetchall()
-    if len(lst) != 0:
-        return "Game name is taken"
-    cur.execute("""SELECT * from professor where email = %s;""", (email,))
-    lst = cur.fetchall()
-    if len(lst) != 0:
-        return "Professor Account already exists! Please login to your account."
-
-    # INSERT STATEMENTS
-    cur.execute("""INSERT INTO professor (email, hashpswd) VALUES (%s, %s);""",(email,hashpassword))
-    print("PROFESSOR ACCOUNT CREATED")
-    cur.execute("""INSERT INTO game (title) VALUES (%s);""", (gameName,))
-    print("CREATED GAME ENTRY")
-    cur.execute("""INSERT INTO professor_game (pid, gid) VALUES ((SELECT pid from professor where email = %s), (SELECT gid from game where title = %s));""", (email, gameName))
-    print("PROFESSOR_GAME RELATION CREATED")
-    conn.commit()
-    return "Professor Account Created!"
+# @app.route("/pcreate/<email>/<password>/<gameName>")
+# def createProfessor(email, password, gameName):
+#     hashpassword = hashed_password(password)
+#     print("CREATED PASSWORD HASH")
+#
+#     # ALREADY EXISTS CHECK
+#     cur.execute("""SELECT * from game where title = %s;""", (gameName,))
+#     lst = cur.fetchall()
+#     if len(lst) != 0:
+#         return "Game name is taken"
+#     cur.execute("""SELECT * from professor where email = %s;""", (email,))
+#     lst = cur.fetchall()
+#     if len(lst) != 0:
+#         return "Professor Account already exists! Please login to your account."
+#
+#     # INSERT STATEMENTS
+#     cur.execute("""INSERT INTO professor (email, hashpswd) VALUES (%s, %s);""",(email,hashpassword))
+#     print("PROFESSOR ACCOUNT CREATED")
+#     cur.execute("""INSERT INTO game (title) VALUES (%s);""", (gameName,))
+#     print("CREATED GAME ENTRY")
+#     cur.execute("""INSERT INTO professor_game (pid, gid) VALUES ((SELECT pid from professor where email = %s), (SELECT gid from game where title = %s));""", (email, gameName))
+#     print("PROFESSOR_GAME RELATION CREATED")
+#     conn.commit()
+#     return "Professor Account Created!"
