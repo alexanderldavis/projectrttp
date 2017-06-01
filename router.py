7eb4b8b3f0bac7f1d7a269d308a34f44a17d37a3
Alexander Davis <alexander.davis54@gmail.com>
2017-06-01 11:08:22 -0500

    change professor login

--- router.py
+++ router.py
@@ -48,10 +48,11 @@ def hashed_password(password):
 
 @app.route("/plogin/<email>/<password>")
 def loginProfessor(email, password):
-    hashpassword = hashed_password(password)
+
     cur.execute("""SELECT hashpswd from professor where email = %s;""", (email,))
     lst = cur.fetchall()
-    if lst[0][0] == hashpassword:
+    # Check password to hashed pass in table
+    if check_password_hash(lst[0][0], password):
         cur.execute("""SELECT * from professor where email = %s;""", (email,))
         lst = cur.fetchall()
         return str(lst)
@@ -61,16 +62,24 @@ def loginProfessor(email, password):
 @app.route("/pcreate/<email>/<password>/<gameName>")
 def createProfessor(email, password, gameName):
     hashpassword = hashed_password(password)
+    print("CREATED PASSWORD HASH")
 
+    # ALREADY EXISTS CHECK
+    cur.execute(""""SELECT * from game where title = %s;""", (gameName,))
+    lst = cur.fetchall()
+    if len(lst) != 0:
+        return "Game name is taken"
     cur.execute("""SELECT * from professor where email = %s;""", (email,))
     lst = cur.fetchall()
     if len(lst) != 0:
         return "Professor Account already exists! Please login to your account."
 
+    # INSERT STATEMENTS
     cur.execute("""INSERT INTO professor (email, hashpswd) VALUES (%s, %s);""",(email,hashpassword))
-    conn.commit()
+    print("PROFESSOR ACCOUNT CREATED")
     cur.execute("""INSERT INTO game (title) VALUES (%s);""", (gameName,))
-    conn.commit()
+    print("CREATED GAME ENTRY")
     cur.execute("""INSERT INTO professor_game (pid, gid) VALUES ((SELECT pid from professor where email = %s), (SELECT gid from game where title = %s));""", (email, gameName))
+    print("PROFESSOR_GAME RELATION CREATED")
     conn.commit()
     return "Professor Account Created!"
