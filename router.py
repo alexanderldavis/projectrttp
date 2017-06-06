53730e25d7530a4e0569f8e827d3626dc251f76f
Alexander Davis <alexander.davis54@gmail.com>
2017-06-06 15:10:36 -0500

    add myaccount.html

--- router.py
+++ router.py
@@ -161,7 +161,7 @@ def getCustomNewspaper(sid):
 ### UPLOADS!!!
 @app.route("/account/")
 def account():
-    return render_template('account.html')
+    return render_template('myaccount.html')
 
 @app.route('/sign_s3/')
 def sign_s3():
