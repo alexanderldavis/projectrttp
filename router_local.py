e92a1c18ab7e06093ec2debbcd2e25e183566f74
odinolav <odinolav@gmail.com>
2017-06-06 13:21:55 -0500

    ui adjustments

--- router_local.py
+++ router_local.py
@@ -115,6 +115,51 @@ def getCustomDashboard(sid):
     charlst = cur.fetchall()
     return render_template('dashboard.html', sid = sid, curid = 1, username="John", description = charlst[0][2])
 
+@app.route("/newspaper/<sid>")
+def getCustomNewspaper(sid):
+    cur.execute("""SELECT * FROM character where cid = """)
+    return "newspaper"
+
+# @app.route("/characterprofile/<sid>")
+#
+# @app.route("/chat/<sid>")
+
+### UPLOADS!!!
+
+
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route("/uploads")
+def uploads():
+    return render_template("uploads.html")
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    print("IN /upload, ")
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        print("about to return")
+        return redirect(url_for('uploaded_file', filename=filename))
+
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 # @app.route("/pcreate/<email>/<password>/<gameName>")
 # def createProfessor(email, password, gameName):
--- static/js/effects.js
+++ static/js/effects.js
@@ -1,51 +1,7 @@
-var userMenu;
-var hoveringUserCircle;
-var hoveringUserMenu;
 
-$(document).ready(function() {
-  userMenu = document.getElementById("usermenu");
-  hoveringUserCircle = false;
-  hoveringUserMenu = false;
-});
-
-function expandUserMenu() {
-  userMenu.style.opacity = "1";
-  userMenu.style.width = "220px";
-  userMenu.style.height = "120px";
-}
-
-function collapseUserMenu() {
-  userMenu.style.opacity = "0";
-  userMenu.style.width = "0";
-  userMenu.style.height = "0";
-}
-
-function hoverUser() {
-  hoveringUserCircle = true;
-  if (!hoveringUserMenu) {
-    expandUserMenu();
-  }
-}
-
-function leaveUser() {
-  hoveringUserCircle = false;
-  setTimeout(function(){
-    if (!hoveringUserMenu) {
-      collapseUserMenu();
-    }
-  }, 300);
-}
-
-function hoverUserMenu() {
-  hoveringUserMenu = true;
-  expandUserMenu();
-}
-
-function leaveUserMenu() {
-  hoveringUserMenu = false;
-  setTimeout(function(){
-    if (!hoveringUserCircle) {
-      collapseUserMenu();
-    }
-  }, 300);
+function hoverSubmit() {
+  /*let submitButton = document.getElementById("submitbutton");
+  submitButton.style.borderRadius = "50px";
+	submitButton.width = "3.5em";
+	submitButton.height = "3.5em";*/
 }
