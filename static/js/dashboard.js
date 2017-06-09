d53540e82515bca45ee51e80f558ea420bf200b0
odinolav <odinolav@gmail.com>
2017-06-09 17:26:43 -0500

    add game box tweaks

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,21 +1,19 @@
+function revertAGB() {
+  // returning gamebox to original state
+  $(this).removeClass("gameformbox");
+  $(this).find("#gameform").addClass("invisible");
+  $(this).data("viewingform", false);
+  $(this).find(".gamelabel").show(100);
+}
+
 $( document ).ready(function() {
 
+  $(".addgamebox").hover(null, revertAGB);
+
   $(".addgamebox").click(function() {
-    if ($(this).data("viewingform")) {
-      /* // returning gamebox to original state
-      $(this).find(".gamelabel").css('opacity', '');
-      $(this).css('border', '');
-      $(this).css('box-shadow', '');
-      $(this).find("#gameform").addClass("invisible");
-      $(this).css('background', '');
-      $(this).data("viewingform", false);
-      $(this).find(".gamelabel").show(100); */
-    } else {
+    if (! $(this).data("viewingform")) {
       // melt away gamelabel, display form
-      //$(this).find(".gamelabel").css('opacity', '0');
-      $(this).css('box-shadow', 'inset 0 0 8px black');
-      $(this).css('border', '4px solid rgb(34, 34, 34)');
-      $(this).css('background','white');
+      $(this).addClass("gameformbox");
       $(this).find("#gameform").removeClass("invisible");
       $(this).data("viewingform", true);
       $(this).find(".gamelabel").hide(100);
