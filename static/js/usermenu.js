b8839f100834fa717cc05bb6d39ac1bcbd98c4b7
James Miller <James Miller>
2017-06-16 17:44:23 -0500

    cleaned up / improved user menu

--- static/js/usermenu.js
+++ static/js/usermenu.js
@@ -1,51 +1,13 @@
-var userMenu;
-var hoveringUserCircle;
-var hoveringUserMenu;
-
 $(document).ready(function() {
-  userMenu = document.getElementById("usermenu");
-  hoveringUserCircle = false;
-  hoveringUserMenu = false;
+  $(function() {
+    $(document).on('click', function(e) {
+        if (e.target.id === 'usercircle') {
+          $('#usermenu').addClass('showusermenu');
+          $('#usermenu').removeClass('hideusermenu');
+        } else {
+          $('#usermenu').removeClass('showusermenu');
+          $('#usermenu').addClass('hideusermenu');
+        }
+    })
+  });
 });
-
-function expandUserMenu() {
-  userMenu.style.opacity = "1";
-  userMenu.style.width = "220px";
-  userMenu.style.height = "120px";
-}
-
-function collapseUserMenu() {
-  userMenu.style.opacity = "0";
-  userMenu.style.width = "0";
-  userMenu.style.height = "0";
-}
-
-function hoverUser() {
-  hoveringUserCircle = true;
-  if (!hoveringUserMenu) {
-    expandUserMenu();
-  }
-}
-
-function leaveUser() {
-  hoveringUserCircle = false;
-  setTimeout(function(){
-    if (!hoveringUserMenu) {
-      collapseUserMenu();
-    }
-  }, 300);
-}
-
-function hoverUserMenu() {
-  hoveringUserMenu = true;
-  expandUserMenu();
-}
-
-function leaveUserMenu() {
-  hoveringUserMenu = false;
-  setTimeout(function(){
-    if (!hoveringUserCircle) {
-      collapseUserMenu();
-    }
-  }, 300);
-}
