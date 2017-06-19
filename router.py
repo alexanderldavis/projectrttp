bca1be73df9c2cf22f37d9d9a2b35b3138fe92e3
James Miller <James Miller>
2017-06-19 14:48:37 -0500

    dashboard testing

--- router.py
+++ router.py
@@ -170,7 +170,6 @@ def getCustomGameChooser(sid):
 
 @app.route("/dashboard/<sid>/<gid>")
 def getCustomDashboard(sid, gid):
-    print('So far so good', file=sys.stderr)
     cur.execute("""SELECT name FROM students where sid = %s;""", (sid,))
     lst = cur.fetchall()
     conn.commit()
@@ -181,16 +180,10 @@ def getCustomDashboard(sid, gid):
     charinfo = cur.fetchall()
     picurl = picurls[0][0]
     charname = picurls[0][1]
-    print('So far so good', file=sys.stderr)
     cur.execute("""SELECT title from game where gid = %s;""", (gid,))
-    print('Post-execution', file=sys.stderr)
     gametitle = cur.fetchall()
-    print('Fetched', file=sys.stderr)
-    conn.commit()
-    print('Committed', file=sys.stderr)
     gametitle = gametitle[0][0]
-    print('Gametitle secured', file=sys.stderr)
-    return render_template('dashboard.html', gid = gid, sid = sid, curid = 1, username=name, gametitle=gametitle, picurl = picurl, charname = charname)
+    return render_template('dashboard.html', gid = gid, sid = sid, curid = 1, username=name, gametitle=gametitle, picurl=picurl, charname=charname)
 
 @app.route("/newspaper/<sid>/<gid>")
 def getCustomNewspaper(sid, gid):
